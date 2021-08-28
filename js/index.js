0db7028f0e1e56f0e98614438801c9ad5207ca8c
tbenjamin1 <tbenpollyl@gmail.com>
2021-08-28 08:08:51 +0300

    styling ui

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const todosContainer = document.querySelector(".todos");
 const completedCount = document.querySelector(".completedCount");
 
 let todos = [];
+let elem =null;
 
 const  changeTheme = ()=> {
   document.body.classList.toggle("light");
@@ -65,8 +66,51 @@ const newTodo = (value) => {
     todo.appendChild(todoText);
     todo.appendChild(todoCross);
 
+    todo.draggable=true;
+    todo.addEventListener("dragstart",(e) =>{
+      e.dataTransfer.effectAllowed ="move";
+      e.dataTransfer.setData("text/plain",null);
+      elem= e.target;
+
+    });
+    todo.addEventListener("dragover",(e) =>{
+      let el1;
+      e.preventDefault();
+      if(e.target.classList.contains("todo")){
+        el1v =  e.target;
+      }else{
+        el1=e.target.parentElement;
+      }
+      if(isBefore(elem,el1)){
+        el1.parentNode.insertBefore(elem,el1);
+      }else{
+        el1.parentNode.insertBefore(elem,el1,nextSibling);
+      }
+
+    });
+    
+    todo.addEventListener("dragend",() =>{
+      elem =null;
+      let index =todos.findIndex((t) => t.value === false);
+        todos.splice(index,1);
+      if(todo.nextSibling){
+        let index1= todos.findIndex(
+   (t) =>t.value === todo.nextSibling,querySelector("p").textContent
+        );
+        todos.splice(index1,0,{
+          value:value,
+          checked: todo.querySelector("input").checked,
+
+        });
+      } else{
+        todos.push({
+          value:value,
+          checked:todo.querySelector("input").checked,
+        });
+      }
+    });
     todosContainer.appendChild(todo);
-  
+
 };
 
  const countCompleted =()=>{
@@ -76,15 +120,35 @@ const newTodo = (value) => {
   
 }
 
+
  const clearCompleted=()=>{
   document.querySelectorAll(".todo").forEach((todo) =>{
     if(todo.querySelectorAll("input").checked){
       todo.remove();
+      console.log("clear")
     }
   });
 }
 const showAll = ()=>{
   document.querySelectorAll(".todo"),forEach((todo) =>{
+todo.style.display="grid";
+console.log("all")
 
   })
-}
+};
+ const filterCompleted =()=>{
+   document.querySelectorAll(".todo").forEach((todo)=>{
+     if(!todo.querySelector("input").checked){
+       todo.display ="none";
+       console.log("complted")
+     }
+   });
+ }
+ const filterActive =()=>{
+   document.querySelectorAll(".todo").forEach((todo)=>{
+     if(todo.querySelectorAll("input").checked){
+       todo.style.display ="none";
+       console.log("active")
+     }
+   });
+ }
